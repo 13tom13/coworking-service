7065bdb4847f709b36c6d98c7ae49e97919e6b77
13tom13 <13tom13@mail.ru>
2024-07-07 01:27:38 +0200

    Added a collection of test queries

--- src/main/java/io/ylab/tom13/coworkingservice/out/utils/aspects/user/UserEditAspect.java
+++ src/main/java/io/ylab/tom13/coworkingservice/out/utils/aspects/user/UserEditAspect.java
@@ -2,8 +2,8 @@ package io.ylab.tom13.coworkingservice.out.utils.aspects.user;
 
 import io.ylab.tom13.coworkingservice.out.entity.dto.UserDTO;
 import io.ylab.tom13.coworkingservice.out.utils.Session;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 
@@ -14,17 +14,15 @@ public class UserEditAspect extends UserAspect {
 
 
     @Pointcut("execution(* io.ylab.tom13.coworkingservice.out.rest.services.UserEditService.editUser(..)) && args(userDTO)")
-    public void editUserPointcut(UserDTO userDTO) {
+    public void editUserPointcut() {
     }
 
-    @Around(value = "editUserPointcut(userDTO)", argNames = "joinPoint,userDTO")
-    public UserDTO logUserEdit(ProceedingJoinPoint joinPoint, UserDTO userDTO) throws Throwable {
+    @AfterReturning(pointcut = "editUserPointcut()", returning = "result")
+    public void logUserEdit(JoinPoint joinPoint, Object result) {
         Optional<UserDTO> user = Session.getInstance().getUser();
         String userEmail = user.isPresent() ? user.get().email() : "anonymous";
         String action = "изменил свои данные";
-        Object result = joinPoint.proceed();
         logger.info("Пользователь с email {} {}.", userEmail, action);
         logToDatabase(userEmail, action);
-        return (UserDTO) result;
     }
 }
