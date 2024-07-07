cc0fd8c92d329613a9af8924c9e2ebe60a6f50bd
13tom13 <13tom13@mail.ru>
2024-07-07 19:07:47 +0200

    fix ValidationExceptionHandlerAspect

--- src/main/java/io/ylab/tom13/coworkingservice/out/utils/aspects/ValidationExceptionHandlerAspect.java
+++ src/main/java/io/ylab/tom13/coworkingservice/out/utils/aspects/ValidationExceptionHandlerAspect.java
@@ -6,18 +6,36 @@ import jakarta.validation.ValidationException;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class ValidationExceptionHandlerAspect {
 
-    @Around("execution(* io.ylab.tom13.coworkingservice.out.rest.servlet.*Servlet.*(..))")
+    @Pointcut("execution(* io.ylab.tom13.coworkingservice.out.rest.servlet.*Servlet.*(..))")
+    public void servletMethods() {
+    }
+
+    @Around("servletMethods()")
     public Object handleValidationException(ProceedingJoinPoint joinPoint) throws Throwable {
         try {
             return joinPoint.proceed();
         } catch (ValidationException e) {
-            HttpServletResponse response = (HttpServletResponse) joinPoint.getArgs()[1];
-            response.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
+            HttpServletResponse response = findHttpServletResponse(joinPoint.getArgs());
+            if (response != null) {
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
+            } else {
+                throw e;
+            }
             return null;
         }
+    }
+
+    private HttpServletResponse findHttpServletResponse(Object[] args) {
+        for (Object arg : args) {
+            if (arg instanceof HttpServletResponse) {
+                return (HttpServletResponse) arg;
+            }
+        }
+        return null;
     }
 }
